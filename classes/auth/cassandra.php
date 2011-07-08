138a094192f8d003001e96aa200f5dfe6400adf4
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-07-08 15:36:24 -0400

    added Cassandra support in auth folder

---
+++ classes/auth/cassandra.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+/**
+ * CASSANDRA Auth driver extended for Useradmin module support.
+ *
+ * @package    Useradmin/Auth
+ * @author     Jean-Nicolas Boulay Desjardins
+ */
+class Auth_CASSANDRA extends Useradmin_Auth_CASSANDRA { }
