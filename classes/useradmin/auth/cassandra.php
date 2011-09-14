eb03c6ac5da234fbddb45da62567043c83d1a1db
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-09-14 03:52:01 -0400

    ...

--- classes/useradmin/auth/cassandra.php
+++ classes/useradmin/auth/cassandra.php
@@ -48,7 +48,8 @@ class Useradmin_Auth_CASSANDRA extends Kohana_Auth_CASSANDRA implements Useradmi
 		$user['username'] = $username;
 		// Loads default driver before extend the results
 		$status = parent::_login($user, $password, $remember);
-echo $user['uuid'];
+echo $user['username'];
+echo '<br/>';
 die(var_dump($user));
 		if($status) 
 		{
