8d694cd1aa755c1b4705c8c560a0372860fe687b
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-09-14 12:49:45 -0400

    add support for none users

--- classes/useradmin/auth/cassandra.php
+++ classes/useradmin/auth/cassandra.php
@@ -59,8 +59,7 @@ class Useradmin_Auth_CASSANDRA extends Kohana_Auth_CASSANDRA implements Useradmi
 			// Successful login
 			// Reset the login failed count	
 			CASSANDRA::selectColumnFamily('Users')->insert($user['uuid'], array('failed_login_count' => 0));
-		} elseif ($user) {
-			die('Yep!');
+		} elseif ($user) {	
 			// Failed login
 			CASSANDRA::selectColumnFamily('Users')->insert($user['uuid'], array(
 											'failed_login_count' => $user['failed_login_count']+1,
