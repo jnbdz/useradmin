3ca078cd2ed8b91cdc181b322ef83de3b82d8524
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-09-20 18:28:27 -0400

    email verified switch from !== to !=

--- classes/controller/useradmin/app.php
+++ classes/controller/useradmin/app.php
@@ -132,7 +132,7 @@ class Controller_Useradmin_App extends Controller {
    public function after() {
       if ($this->auto_render === TRUE) {
 
-	 if(Auth::instance()->logged_in() && Auth::instance()->get_user()->email_verified !== 'true')
+	 if(Auth::instance()->logged_in() && Auth::instance()->get_user()->email_verified != 'true')
 	 {
 		Message::add('error', __('You need to confirm your email. Resend :confirmation_email.', array(
 						':confirmation_email'   => HTML::anchor('/user/send_confirmation_email/', __('confirmation email')),
