43b6080009949e58f964a91d9eebbdacb9fc8232
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-09-23 00:03:56 -0400

    ...

--- classes/useradmin/auth/cassandra.php
+++ classes/useradmin/auth/cassandra.php
@@ -31,7 +31,8 @@ class Useradmin_Auth_CASSANDRA extends Kohana_Auth_CASSANDRA implements Useradmi
 				{	
 					$log->add(Log::ERROR, 'There was a conflic with the username and/or email. UUID: '.$user['uuid'].' username: '.$user['username'].' email: '.$user['email']);
 					$this->request->redirect('error/conflic');
-					return;
+					$user = FALSE;
+					break;
 				}
 				$i++;
 			}
