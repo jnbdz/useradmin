2c17e2791a482cb64489a1899079adf4a3d8f59a
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-10-13 03:42:39 -0400

    Added new config for init file for better navigation.

--- init.php
+++ init.php
@@ -1,4 +1,10 @@
 <?php
+Route::set('user/action', 'user/action(/<action>(/<id>))')
+	->defaults(array(
+		'controller' => 'user',
+		'action'     => 'login',
+	));
+
 Route::set('user/provider', 'user/provider(/<provider>)', array('provider' => '.+'))
 	->defaults(array(
 		'controller' => 'user',
@@ -11,6 +17,12 @@ Route::set('user/provider_return', 'user/provider_return(/<provider>)', array('p
 		'controller' => 'user',
 		'action'     => 'provider_return',
 		'provider'       => NULL,
+	));
+
+Route::set('user', 'user/<username>')
+	->defaults(array(
+		'controller' => 'user',
+		'action'     => 'profile',
 	));
 
 // Static file serving (CSS, JS, images)
